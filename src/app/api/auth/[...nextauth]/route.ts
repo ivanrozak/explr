7c10f73c72b299e9be774af610eed2b7ced4e983
ivanrozak <ivan.rozak@yesdok.com>
2024-10-14 01:09:21 +0700

    adding Credential providers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
+import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/lib/prisma";
 
 const handler = NextAuth({
@@ -9,6 +11,35 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user?.hashedPassword
+        );
+
+        if (isCorrectPassword) {
+          return user;
+        }
+
+        return null;
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
