b3da4fb9a17d1007ee3304378b5c4a304d4de950
ivanrozak <ivan.rozak@yesdok.com>
2024-10-17 15:04:35 +0700

    handle error build

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 
 const Page = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const searchParams = useSearchParams();
-  const errorMessage = searchParams.get("error");
   return (
     <div className="flex flex-col max-w-sm gap-4 mx-auto text-center p-6">
       <h1 className="text-xl">Sign In Page</h1>
@@ -24,7 +22,9 @@ const Page = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      {errorMessage && <p>error on: {errorMessage}</p>}
+      <Suspense>
+        <ErrorMessage />
+      </Suspense>
       <button onClick={() => signIn("credentials", { email, password })}>
         Sign In with Credentials
       </button>
@@ -35,3 +35,9 @@ const Page = () => {
 };
 
 export default Page;
+
+const ErrorMessage = () => {
+  const searchParams = useSearchParams();
+  const errorMessage = searchParams.get("error");
+  return <>{errorMessage && <p>error on: {errorMessage}</p>}</>;
+};
