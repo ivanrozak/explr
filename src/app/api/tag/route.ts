a0be9666bee96b8624908aaa3cd6adf137b2097a
ivanrozak <ivan.rozak@yesdok.com>
2024-10-18 23:40:02 +0700

    adding tag API

--- src/app/api/tag/route.ts
+++ src/app/api/tag/route.ts
@@ -29,7 +29,7 @@ export const GET = async (req: NextRequest) => {
     const response = await prisma.tag.findMany({
       where: {
         name: {
-          contains: query,
+          contains: query || "",
         },
       },
       orderBy: {
